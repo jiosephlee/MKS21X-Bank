c089b9a25933a7ef3fc3b72eec3665601e67b8fa
Joseph Lee <jlee01@stuy.edu>
2018-10-03 22:26:33 -0400

    fixed and tested Driver code.

--- Driver.java
+++ Driver.java
@@ -5,38 +5,39 @@ public class Driver{
 
         System.out.println("Balance: $" + yeet.getBalance());
         System.out.println("Account ID: " + yeet.getAccountID());
-        System.out.println("Account ID, Balance" + yeet);
+        System.out.println("Account ID, Balance" + "   " + yeet);
+        System.out.println("\n");
 
         yeet.setPassword("whomstv");
 
         if(yeet.deposit(111.0)){
           System.out.println("$" + 111.0 + " Deposit Success! :) ");
         }else{
-          System.out.println("$" + 111.0 + "Deposit Failure! :( ");}
+          System.out.println("$" + 111.0 + " Deposit Failure! :( ");}
         System.out.println("Balance: $" + yeet.getBalance());
 
         if(yeet.deposit(-111.0)){
-          System.out.println("$" + -111.0 + "Deposit Success! :) ");
+          System.out.println("$" + -111.0 + " Deposit Success! :) ");
         }else{
-          System.out.println("$" + -111.0 + "Deposit Failure! :( ");}
+          System.out.println("$" + -111.0 + " Deposit Failure! :( ");}
         System.out.println("Balance: $" + yeet.getBalance());
 
         if(yeet.withdraw(111.0)){
-          System.out.println("$" + 111.0 + "Withdraw Success! :) ");
+          System.out.println("$" + 111.0 + " Withdraw Success! :) ");
         }else{
-          System.out.println("$" + 111.0 + "Withdraw Failure! :( ");}
+          System.out.println("$" + 111.0 + " Withdraw Failure! :( ");}
         System.out.println("Balance: $" + yeet.getBalance());
 
         if(yeet.withdraw(-111.0)){
-          System.out.println("$" + -111.0 + "Withdraw Success! :) ");
+          System.out.println("$" + -111.0 + " Withdraw Success! :) ");
         }else{
-          System.out.println("$" + -111.0 + "Withdraw Failure! :( ");}
+          System.out.println("$" + -111.0 + " Withdraw Failure! :( ");}
           System.out.println("Balance: $" + yeet.getBalance());
 
         if(yeet.withdraw(11111.0)){
-          System.out.println("$" + 11111.0 + "Withdraw Success! :) ");
+          System.out.println("$" + 11111.0 + " Withdraw Success! :) ");
         }else{
-          System.out.println("$" + 11111.0 + "Withdraw Failure! :( ");}
+          System.out.println("$" + 11111.0 + " Withdraw Failure! :( ");}
           System.out.println("Balance: $" + yeet.getBalance());
     }
 }
